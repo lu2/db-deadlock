879ea6ee209aad34fd0f9fcc2a12d9936392d7de
Ludvik Haltuf <ludvik.haltuf@gmail.com>
2023-02-19 02:40:33 +0100

    added spring boot skeleton

---
+++ src/main/java/net/haltuf/playground/dbdeadlock/DbDeadlockApplication.java
@@ -0,0 +1,13 @@
+package net.haltuf.playground.dbdeadlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbDeadlockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbDeadlockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/haltuf/playground/dbdeadlock/DbDeadlockApplicationTests.java
@@ -0,0 +1,13 @@
+package net.haltuf.playground.dbdeadlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbDeadlockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
